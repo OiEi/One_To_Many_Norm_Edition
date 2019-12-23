f904c15393c1feae3fef7ad06bac848af7df41a1
OiEi <savsemleviy@gmail.com>
2019-12-23 18:23:49 +0300

    first

---
+++ EmployeeDataAcces/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace EmployeeDataAcces
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext() : base("EmployeeDB")
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        /*public DbSet<EmployeePasport> Pasports { get; set; }*/
+
+
+    }
+}
